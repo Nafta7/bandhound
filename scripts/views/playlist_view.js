fed161c5bcc828e53489455cb16b0f8add391b4c
Nefla <neflasan@gmail.com>
2015-04-28 20:10:05 -0300

    refactor find similar artists

--- scripts/views/playlist_view.js
+++ scripts/views/playlist_view.js
@@ -1,5 +1,6 @@
 var $ = require('jquery'),
     Backbone = require('backbone'),
+    ArtistFinder = require('../models/artist_finder'),
     PlayerView = require('./player_view');
 
 Backbone.$ = window.$;
@@ -12,14 +13,17 @@ module.exports = Backbone.View.extend({
       el: $('#player-controls'),
       playlist: this
     });
+    this.$playlist = this.$el.find('table');
+    this.page = 1;
   },
 
   events: {
-    "click tr": "playSong"
+    "click tr": "playSong",
+    "click #load-more": "loadMore"
   },
 
   search: function(){
-    this.$el.empty();
+    this.$playlist.children('tbody').remove();
     var self = this;
     setTimeout(function(){
       var list = "<tr " + "data-youtube-id='I67ThDYhk7g'";
@@ -37,19 +41,20 @@ module.exports = Backbone.View.extend({
   },
 
   clear: function(){
-    this.$el.children('tbody').remove();
+    this.$playlist.children('tbody').remove();
     this.selected = null;
     this.playing = null;
   },
 
-  playSong: function(e){
-    if (this.selected != null) {
-      this.selected.removeClass('current');
-    }
-    this.selected = $(e.currentTarget);
-    this.selected.addClass('current');
+  loadMore: function(){
+    this.loading();
+    this.artistFinder.findSimilar({
+      artist: this.artist, page: ++this.page, callback: this.loaded });
+  },
 
+  playSong: function(e){
     this.playing = $(e.currentTarget);
+    this.selectNewTrack();
     var track  = $(e.currentTarget).children('td')[0].innerText;
     var artist = $(e.currentTarget).children('td')[1].innerText;
     var id = $(e.currentTarget).attr('data-youtube-id');
@@ -87,5 +92,31 @@ module.exports = Backbone.View.extend({
       title : this.playing.children('td')[0].innerText,
       id    : this.playing.attr('data-youtube-id')
     };
+  },
+
+  findSimilarArtists: function(artist){
+    this.clear();
+    this.loading();
+    this.artistFinder = new ArtistFinder();
+    this.artistFinder.findSimilar({artist: artist, callback: this.loaded});
+    this.enabled = true;
+  },
+
+  loading: function(){
+    // Display load spinner
+    var $loader = $('#loader-container');
+    $loader.removeClass('hidden');
+
+    // Hide load more button
+    var $loadMore = $('#load-more');
+    $loadMore.addClass('hidden');
+  },
+
+  loaded: function(){
+    var $loader = $('#loader-container');
+    $loader.addClass('hidden');
+
+    var $loadMore = $('#load-more');
+    $loadMore.removeClass('hidden');
   }
 });
--- scripts/views/search_view.js
+++ scripts/views/search_view.js
@@ -1,6 +1,5 @@
 var $ = require('jquery'),
     Backbone = require('backbone'),
-    ArtistFinder = require('../models/artist_finder'),
     PlaylistView = require('./playlist_view');
 Backbone.$ = window.$;
 
@@ -15,26 +14,8 @@ module.exports = Backbone.View.extend({
 
   keyPressHandler: function(e){
     if (e.keyCode == 13){
-      // this.playlist.search();
-      this.searchSimilarArtists(this.$el.children('#search-artist').val());
+      $('#hero').addClass('hidden');
+      this.playlist.findSimilarArtists(this.$el.children('#search-artist').val());
     }
-  },
-
-  searchSimilarArtists: function(artist){
-    $('#hero').addClass('hidden');
-    this.playlist.clear();
-    $loader = $('#loader-container');
-    $loader.removeClass('hidden');
-    $loadMore = $('#load-more');
-    $loadMore.addClass('hidden');
-    var artistFinder = new ArtistFinder(artist);
-    var hideLoader = function(){
-      $loader.addClass('hidden');
-      $loadMore.removeClass('hidden');
-    };
-    artistFinder.findSimilar({artist: artist, callback: hideLoader});
-    this.playlist.enabled = true;
-    // $ldMore = '<div class="col-1-1"><input type="submit" value="Load more"></div>';
-    // $('#content-main').append($ldMore);
   }
 });
