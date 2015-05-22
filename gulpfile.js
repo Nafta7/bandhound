9fb8d460ea637664937673c5fee399e52c759c43
Nefla <neflasan@gmail.com>
2015-05-22 16:21:29 -0300

    Update gulp with livereload and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,16 @@ var jade = require('gulp-jade');
 var sourcemaps = require('gulp-sourcemaps');
 var uncss = require ('gulp-uncss');
 
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var assign = require('lodash.assign');
+
+var livereload = require('gulp-livereload');
+
 // Source directories
 var dirScripts   = 'scripts/';
 var dirStyles    = 'styles/';
@@ -33,7 +43,9 @@ var dirVendor = {
   }
 };
 
-
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = dirDest.root;
+var LIVERELOAD_PORT = 35729;
 
 /*-------------------------------
 --------DEVELOPMENT TASKS--------
@@ -68,6 +80,61 @@ gulp.task('jade', function() {
 });
 
 
+// add custom browserify options here
+var customOpts = {
+  entries: ['./' + dirScripts + 'app.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source(dirScripts + 'bundle.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest(dirDest.root));
+}
+
+
+
+function startExpress(){
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 4002}));
+  app.use(express.static(dirDest.root));
+  app.listen(4000);
+}
+
+var tinylr;
+function startLivereload(){
+  tinylr = require('tiny-lr')();
+  tinylr.listen(4002);
+}
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(dirDest.root, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
 
 /*-------------------------------
 -----------BUILD TASKS-----------
@@ -90,20 +157,24 @@ gulp.task('buildcss', function(){
   return gulp.src([vendorcss, maincss])
     .pipe(concat('all.css'))
     .pipe(rename('all.min.css'))
-    // .pipe(uncss({
-    //   html: [
-    //       'www/index.html'
-    //   ]}))
-    // .pipe(gulp.dest('./out'))
     .pipe(minifycss())
     .pipe(gulp.dest(dirDest.styles));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch(dirStyles + '**/*.sass', ['sass']);
-  gulp.watch(dirTemplates + '**/*.jade', ['jade']);
-});
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'jade', 'watch']);
+// gulp.task('default', ['lint', 'sass', 'jade', 'watch']);
+
+// Watch Files For Changes
+gulp.task('watch', function(){
+  startExpress();
+  startLivereload();
+
+  gulp.watch(dirStyles + '**/*.sass', ['sass']);
+  gulp.watch(dirTemplates + '**/*.jade', ['jade']);
+  gulp.watch(dirScripts + '**/*.js', ['js'] );
+
+  gulp.watch(dirDest.styles + '**/*.css', notifyLiveReload);
+  gulp.watch(dirDest.scripts + '**/*.js', notifyLiveReload);
+  gulp.watch(dirDest.templates + '**/*.html', notifyLiveReload);
+});
