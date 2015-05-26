92cb80254ede3c593a0b758d00a6538ba6495faa
Nefla <neflasan@gmail.com>
2015-05-26 03:11:09 -0300

    implement custom pagination

--- scripts/models/artist_finder.js
+++ scripts/models/artist_finder.js
@@ -5,38 +5,53 @@ var TrackFetcher = require('./track_fetcher');
 
 module.exports = Backbone.Model.extend({
   initialize: function(){
-    self = this;
   },
 
   findSimilar: function(options){
-    self = this;
     var fm = new LastFM({
       apiKey    : 'b26bc1d28ac087bd6ee4bbaa4fe186ee',
       apiSecret : '4b9340fc31f703706649cc8f617a9939'
     });
     var artist;
 
-    options.limit = options.limit || 10;
-    options.page  = options.page  || 1;
+    options.limit = options.limit || 5;
+    options.page  = options.page  || 0;
+
+    /*
+    Since the Last.FM API doesn't provide a page parameter for this action,
+    which is, Artist.GetSimilar, we're going to implement a custom pagination
+    by ourselves.
+    */
+    var pageSize = options.page * 5;     // Page size
+    var tail = options.limit + pageSize; // Tail
+    var similar;                         // Stores pagination result
 
     fm.artist.getSimilar({
-      artist: options.artist, limit: options.limit, page: options.page
-    },{success: function(data){
-      trackFetcher = new TrackFetcher();
-      data.similarartists.artist.forEach(function(dataArtist){
-        artist = new Artist({
-          fm: fm,
-          trackFetcher: trackFetcher
-        });
-        artist.topTracks({mbid: dataArtist.mbid, limit: 2});
-      });
+      artist: options.artist,
+      limit: tail }, {
+        success: function(data){
+          // Pagination of results
+          similar = data.similarartists.artist.slice(pageSize);
 
-      setTimeout(function(){
-        options.callback();
-      }, 500);
+          if (similar instanceof Array & similar.length > 2) {
+            trackFetcher = new TrackFetcher();
+            similar.forEach(function(dataArtist){
+              artist = new Artist({
+                fm: fm,
+                trackFetcher: trackFetcher
+              });
+              artist.topTracks({mbid: dataArtist.mbid, limit: 2});
+            });
 
-    }, error: function(code, message){
-      console.log(message);
+            setTimeout(function(){
+              options.callback(options.self);
+            }, 500);
+          }
+          else {
+            options.errorHandler(options.self, "001", "Insuficient similar artists.");
+          }
+        }, error: function(code, message){
+            options.errorHandler(options.self, code, message);
     }});
   }
 });
