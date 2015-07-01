222df523a4783fbe88dc6c129b256b950586a46d
Nefla <neflasan@gmail.com>
2015-07-01 17:14:04 -0300

    expose jquery to global scope

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ var Backbone = require('backbone');
 var MainView = require('./views/main_view.js');
 
 Backbone.$ = $;
+global.$ = $;
 
 $(document).ready(function(){
   var mainView = new MainView({el: $('#discover')});
