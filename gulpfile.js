2092634a2d26d2aac07f422fd8741788dc1efead
Nefla <neflasan@gmail.com>
2015-07-04 14:06:37 -0300

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,38 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+plugins.uglify = require('gulp-uglify');
 plugins.browserSync = require('browser-sync');
+plugins.minifycss = require('gulp-minify-css');
 
 var gulpAug = require('gulp-augments')(gulp);
 
 var path = {
-  styles    : { src: 'styles/',  dest: 'publish/styles/' },
-  scripts   : { src: 'scripts/', dest: 'publish/scripts/' },
-  templates : { src: 'views/',   dest: 'publish' },
+  styles      : { src: 'styles/',            dest: 'www/styles/' },
+  scripts     : { src: 'scripts/',           dest: 'www/scripts/' },
+  templates   : { src: 'views/',             dest: './' },
   vendor: {
-    scripts:    { src: 'vendor/scripts/',    dest: 'publish/scripts/' },
-    components: { src: 'vendor/components/', dest: 'publish/styles/'}
+    scripts   : { src: 'vendor/scripts/',    dest: 'www/scripts/' },
+    components: { src: 'vendor/components/', dest: 'www/styles/'}
   }
 };
 
 var toska = require('toska');
-var tasks = toska.mirror('tasks', gulp, {path: path, $: plugins});
+var tasks = toska('tasks', {gulp: gulp, path: path, $: plugins});
 
-gulp.task('default', tasks.build);
+var build = Object.keys(tasks.build).map(function(task){
+  gulp.task(task, tasks.build[task]);
+  return task;
+});
 
-gulp.task('serve', tasks.build, function(){
+var deploy = Object.keys(tasks.deploy).map(function(task){
+  gulp.task(task, tasks.deploy[task]);
+  return task;
+});
+
+gulp.task('build', build);
+gulp.task('deploy', build.concat(deploy));
+
+gulp.task('serve', build, function(){
   plugins.browserSync.init({ server: "./" });
    gulp.watch(path.styles.src + '**/*.sass', ['compile:sass']);
    gulp.watch(path.templates.src + '**/*.jade', ['compile:jade']);
