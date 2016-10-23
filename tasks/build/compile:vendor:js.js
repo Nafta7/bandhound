bc9d993adbbb8f52df4b245490795cf9dfba353e
Nefla <neflasan@gmail.com>
2016-10-23 18:45:53 -0200

    Remove files from the old backbone version

--- tasks/build/compile:vendor:js.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(gulp, path, $){
-  return function(){
-    gulp.src(path.vendor.scripts.src + '**/*.js')
-    .pipe($.concat('vendor.all.js'))
-    .pipe(gulp.dest(path.vendor.scripts.dest))
-    .pipe($.browserSync.stream());
-  }
-}
--- tasks/build/copy:icons.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(gulp, path){
-  return function(){
-    gulp.src(path.vendor.components.src + 'open-iconic/**/*')
-    .pipe(gulp.dest(path.vendor.components.dest + '/open-iconic'));
-  };
-}
