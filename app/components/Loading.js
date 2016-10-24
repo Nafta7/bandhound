cf23a8fee6554d8050aeb0697eadebcbc72d1dcc
Nefla <neflasan@gmail.com>
2016-10-23 22:10:07 -0200

    Loading component support for size

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,7 +1,13 @@
 import React from 'react'
+const PropTypes = React.PropTypes
 
 const Loading = (props) =>
-  <div id="main-loader" className="loader">
+  <div id="main-loader"
+       className={props.size ? `${props.size}-loader` : 'loader'}>
   </div>
+
+Loading.propTypes = {
+  size: PropTypes.string
+}
 
 export default Loading
