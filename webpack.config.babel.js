18c19937610bf57c83562cadc7847d88a6af00b1
Nefla <neflasan@gmail.com>
2016-10-24 13:52:56 -0200

    refactor webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack')
 const isDev = process.argv.indexOf('-p') === -1
+// const isDev = process.env.NODE_ENV !== 'production'
 const path = require('path')
 
-const HtmlWpp = require('html-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const extractCSS = new ExtractTextPlugin('site.css', {disable: isDev})
+const pluginExtractCSS = new ExtractTextPlugin('site.css', {disable: isDev})
+
 const sassLoader = [
   'css'+(isDev? '?sourceMap=true': ''),
   'sass'+(isDev? `?outputStyle=expanded&sourceMap=true&sourceMapContents=true`: '')
@@ -13,12 +15,34 @@ const sassLoader = [
 if(isDev)
   sassLoader.unshift('style')
 
-const htmlConfig = new HtmlWpp({
+let plugins = []
+
+const pluginHTMLWebpack = new HTMLWebpackPlugin({
   template: `${__dirname}/app/index.html`,
   filename: 'index.html',
   favicon: `${__dirname}/app/favicon.ico`,
   inject: 'body'
 })
+
+let pluginProduction = new webpack.DefinePlugin({
+  'process.env': {
+    // This has effect on the react lib size
+    'NODE_ENV': JSON.stringify('production'),
+  }
+})
+
+let pluginOccurenceOrder = new webpack.optimize.OccurenceOrderPlugin()
+
+let pluginUglify = new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+  })
+plugins.push(pluginProduction)
+plugins.push(pluginOccurenceOrder)
+plugins.push(pluginHTMLWebpack)
+plugins.push(pluginExtractCSS)
+if (!isDev) {
+  plugins.push(pluginUglify)
+}
 
 module.exports = {
   devtool: 'source-map',
@@ -41,22 +65,19 @@ module.exports = {
       },
       {
         test: [/\.scss$/, /\.sass$/],
-        loader: extractCSS.extract(sassLoader)
+        loader: pluginExtractCSS.extract(sassLoader)
       },
       {
         test: /\.json$/,
         loader: 'json-loader'
       },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
     ]
   },
-  plugins: [new webpack.DefinePlugin({
-    'process.env': {
-      // This has effect on the react lib size
-      'NODE_ENV': JSON.stringify('production'),
-    }
-  }), new webpack.optimize.OccurenceOrderPlugin(), htmlConfig, extractCSS, isDev? [] : new webpack.optimize.UglifyJsPlugin({
-    minimize: true
-    })]
+  plugins: plugins
 }
