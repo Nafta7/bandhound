d95a1b333e73ccec833abad15f6915f22aa51935
Nefla <neflasan@gmail.com>
2016-10-24 00:19:57 -0200

    Allow main Loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,10 +1,15 @@
 import React from 'react'
 const PropTypes = React.PropTypes
 
-const Loading = (props) =>
-  <div id="main-loader"
-       className={props.size ? `${props.size}-loader` : 'loader'}>
-  </div>
+const Loading = (props) => {
+  let classy = props.size ? `${props.size}-loader` : 'loader'
+  classy += props.main ? ' main-loader' : ''
+
+  return (
+    <div className={classy}>
+    </div>
+  )
+}
 
 Loading.propTypes = {
   size: PropTypes.string
