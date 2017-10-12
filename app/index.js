35329d81c47370cc0cc688333c3de58628577847
Nefla <neflasan@gmail.com>
2017-10-12 16:01:38 -0300

    Switch require for import in css load

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
-require('./styles/pages/index.sass')
+import './styles/pages/index.sass'
 
 ReactDOM.render(routes, document.getElementById('app'))
